aa8d2460b3d284f13c13d988fe20c426901c4bcf
Hongda Hu <jerrery520@gmail.com>
2017-06-16 00:26:18 +0800

    [jerry] refactor the todo column & the todolist issue

--- db/migrate/20170615144106_create_todos.rb
+++ db/migrate/20170615144106_create_todos.rb
@@ -1,9 +1,12 @@
 class CreateTodos < ActiveRecord::Migration[5.0]
   def change
-    create_table :todos, comment: 'the tasks' do |t|
+    create_table :todos, comment: 'the todo tasks' do |t|
       t.string :title, null: false, default: '', comment: 'task title'
       t.text :description
       t.integer :priority, null: false, default: 0, comment: 'task priority'
+
+      t.references :user,  null: false, default: 0, comment: 'create user'
+      t.references :todolist, null: false, default: 0
       t.references :project, null: false, default: 0
       t.references :tag, null: false, default: 0
       t.timestamps
--- db/migrate/20170615160233_create_todolists.rb
+++ db/migrate/20170615160233_create_todolists.rb
@@ -1,7 +1,9 @@
 class CreateTodolists < ActiveRecord::Migration[5.0]
   def change
     create_table :todolists do |t|
-
+      t.string :uid, null: false, default: '', limit: 32
+      t.string :name, null: false, default: '', limit: 50
+      t.references :project, null: false, default: 0
       t.timestamps
     end
   end
